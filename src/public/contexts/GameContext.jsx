e3a756d69b8f6b64a1edd1f628048e70d635517d
Sam R-P <srenshawpanting@gmail.com>
2024-07-11 01:22:47 +0100

    Create GameContext.jsx

---
+++ src/public/contexts/GameContext.jsx
@@ -0,0 +1,21 @@
+// Store difficulties and modifiers and multipliers
+
+// Difficulties:
+// Easy, Normal, Hard, Insane
+// Cadet, Soldier, Veteran, Elite, Legendary
+// Rookie, Novice, Expert, Master, Grandmaster
+// Apprentice, Journeyman, Artisan, Master, Grandmaster
+// Beginner, Intermediate, Advanced, Expert, Master
+// Trainee, Recruit, Soldier, Veteran, Elite
+// Novice, Adept, Expert, Master, Grandmaster
+// Cosmonaut,
+
+// First Flight, Cruising Altitude, Perilous Expedition, One way journey
+// Lift Off, Cruising Altitude, Perilous Encounters, One way journey
+
+// First Flight, Orbital Patrol, Perilous Expedition, One way journey
+
+// Modifiers:
+// - No shop
+// - I'm broke (no money)
+// -
