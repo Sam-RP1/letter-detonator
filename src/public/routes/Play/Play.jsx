608be9a072758678faacd9be2a620a517da9fde4
Sam R-P <srenshawpanting@gmail.com>
2024-07-13 11:44:29 +0100

    Create Play.jsx

---
+++ src/public/routes/Play/Play.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Play = () => {
+  return (
+    <div>
+      <p>Press the "Play" button to start the game.</p>
+      <button>Play</button>
+    </div>
+  );
+};
+
+export default Play;
